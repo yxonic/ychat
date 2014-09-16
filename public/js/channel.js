e58f0622f4daa943dc8ad0bd39f9fe45b24a1182
Yin Yu <yxonic@gmail.com>
2014-09-16 20:08:56 +0800

    User can now select channel from root page.

---
+++ public/js/channel.js
@@ -0,0 +1,20 @@
+$(document).one('pageshow', function(event) {
+    setTimeout(function () {
+        $('.channel').focus();
+    }, 300);
+});
+
+var textbox = $('.channel');
+
+textbox.keyup(function(e) {
+    if (e.keyCode == 13 && textbox.val().length != 0) {
+        $('#ok').trigger('click');
+    }
+});
+
+$('#ok').click(function() {
+    var channel = textbox.val();
+    if (channel.length == 0)
+        channel = "lobby";
+    window.location.href = "/" + channel;
+});
