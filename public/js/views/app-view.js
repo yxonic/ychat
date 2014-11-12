afccb7acdfefc260aea174e061755dcc2532b920
Yin Yu <yxonic@gmail.com>
2014-11-12 12:22:19 +0800

    Print time between long breaks.

--- public/js/views/app-view.js
+++ public/js/views/app-view.js
@@ -2,7 +2,23 @@ var app = app || {};
 
 (function ($) {
     'use strict';
-
+    
+    var format = function(date, full) {
+        var str = '';
+        if (full) {
+            var text = date.getFullYear() + '-'
+                + (date.getMonth() + 1) + '-'
+                + date.getDate() + ' ';
+            str += text;
+        }
+        var time = date.getHours() + ':'
+            + date.getMinutes() + ':'
+            + date.getSeconds();
+        str += time;
+        return str;
+    }
+    var duration = 180000;
+   
     app.AppView = Backbone.View.extend({
 
         el: '#main',
@@ -24,6 +40,7 @@ var app = app || {};
             this.$client.subscribe('/' + app.uid, function (msg) {
                 msg.msgs.forEach(function(msg) {
                     msg.history = true;
+                    app.mark = app.time;
                     app.time = msg.time;
                     app.msgs.create(msg);
                 });
@@ -42,19 +59,43 @@ var app = app || {};
         addOne: function (msg) {
             var view = new app.MsgView({ model: msg });
             if (msg.toJSON().history) {
+                var first = app.msgs.at(1);
+                if (!first) first = msg.attributes;
+                else first = first.attributes;
+                if (first.time - msg.attributes.time > duration) {
+                    var date = new Date(app.mark);
+                    var pdate = new Date(msg.attributes.time);
+                    var datestr = '';
+                    if (date.getDate() != (new Date()).getDate())
+                        datestr += format(date, true);
+                    else
+                        datestr += format(date);
+                    this.$list.prepend("<li style='text-align:right'>"
+                                       + datestr
+                                       + "</li>").listview('refresh');
+                }
                 this.$list.prepend(view.render().el).listview('refresh');
             } else {
+                var last = app.msgs.at(app.msgs.length - 2).attributes;
+                console.log(app.msgs);
+                if (msg.attributes.time - last.time > duration) {
+                    var date = new Date(last.time);
+                    var pdate = new Date(msg.attributes.time);
+                    var datestr = '';
+                    if (date.getDate() != pdate.getDate())
+                        datestr += format(pdate, true);
+                    else
+                        datestr += format(pdate);
+                    this.$list.append("<li style='text-align:right'>"
+                                      + datestr
+                                      + "</li>").listview('refresh');
+                }
                 this.$list.append(view.render().el).listview('refresh');
-                var last_li = $("ul li:last-child").offset().top;
+                var last_li = $("ul li:last-child");
                 setTimeout(function () {
-                    $.mobile.silentScroll(last_li);
+                    $.mobile.silentScroll(last_li.offset().top);
                 }, 50);
             }
-        },
-
-        addAll: function () {
-            this.$list.html('');
-            app.msgs.each(this.addOne, this);
         },
 
         newAttributes: function () {
